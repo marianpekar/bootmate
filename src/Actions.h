43ee193c010d8dd0d1afda2fe39e9ec3d1a78019
Marian Pekár <marian.pekar@gmail.com>
2023-05-01 19:15:13 +0200

    Add map of virtual keys for PressKeyAction

--- src/Actions.h
+++ src/Actions.h
@@ -46,19 +46,66 @@ struct WriteAction : public Action
     }
 };
 
+static std::map<std::string, int> virtualKeys {
+    { "return", VK_RETURN },
+    { "enter", VK_RETURN },
+    { "shift", VK_SHIFT },
+    { "ctrl", VK_CONTROL },
+    { "control", VK_CONTROL},
+    { "alt", VK_MENU },
+    { "backspace", VK_BACK },
+    { "delete", VK_DELETE },
+    { "tab", VK_TAB },
+    { "escape", VK_ESCAPE },
+    { "space", VK_SPACE },
+    { "left", VK_LEFT },
+    { "right", VK_RIGHT },
+    { "up", VK_UP },
+    { "down", VK_DOWN },
+    { "home", VK_HOME },
+    { "end", VK_END },
+    { "pageup", VK_PRIOR },
+    { "pagedown", VK_NEXT },
+    { "insert", VK_INSERT },
+    { "f1", VK_F1 },
+    { "f2", VK_F2 },
+    { "f3", VK_F3 },
+    { "f4", VK_F4 },
+    { "f5", VK_F5 },
+    { "f6", VK_F6 },
+    { "f7", VK_F7 },
+    { "f8", VK_F8 },
+    { "f9", VK_F9 },
+    { "f10", VK_F10 },
+    { "f11", VK_F11 },
+    { "f12", VK_F12 },
+    { "numpad0", VK_NUMPAD0 },
+    { "numpad1", VK_NUMPAD1 },
+    { "numpad2", VK_NUMPAD2 },
+    { "numpad3", VK_NUMPAD3 },
+    { "numpad4", VK_NUMPAD4 },
+    { "numpad5", VK_NUMPAD5 },
+    { "numpad6", VK_NUMPAD6 },
+    { "numpad7", VK_NUMPAD7 },
+    { "numpad8", VK_NUMPAD8 },
+    { "numpad9", VK_NUMPAD9 },
+    { "multiply", VK_MULTIPLY },
+    { "add", VK_ADD },
+    { "separator", VK_SEPARATOR },
+    { "subtract", VK_SUBTRACT },
+    { "decimal", VK_DECIMAL },
+    { "divide", VK_DIVIDE },
+};
 struct PressKeyAction : public Action
 {
-    std::string key;
-    PressKeyAction(std::string key) : Action(ActionTypeName.pressAction), key(key) {}
+    std::string keyName;
+    PressKeyAction(std::string keyName) : Action(ActionTypeName.pressAction), keyName(keyName) {}
 
     void Execute() override
     {
         INPUT input{ 0 };
         input.type = INPUT_KEYBOARD;
-        if (key == "return" || key == "enter")
-        {
-            input.ki.wVk = VK_RETURN;
-        }
+        input.ki.wVk = virtualKeys[keyName];
         SendInput(1, &input, sizeof(INPUT));
     }
 };
